1e8f5b1166e1ed79f2096408dcf47d265e4b21da
AJPascoe <pascoe312@gmail.com>
2021-08-27 15:18:08 +0100

    added UI-Icons

--- src/Components/topbar/Topbar.jsx
+++ src/Components/topbar/Topbar.jsx
@@ -1,8 +1,25 @@
 import "./topbar.scss"
+import {Person, Mail} from "@material-ui/icons"
 
 export default function topbar() {
     return (
         <div className="topbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">AJP</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>+447498969659</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>adamj.pascoe@protonmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+               
+                </div>
+            </div>
             
         </div>
     )
