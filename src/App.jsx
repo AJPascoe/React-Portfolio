eddb9321fb001ff9b64caa4ee10bbe46a9dfc45f
AJPascoe <pascoe312@gmail.com>
2021-08-27 16:58:18 +0100

    Hamburger menu side menu links added

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import Testimonials from "./Components/testimonials/Testimonials";
 import Contact from "./Components/contact/Contact";
 import "./app.scss";
 import {useState} from "react";
+import Menu from "./Components/menu/Menu";
 
 function App() {
     const [menuOpen, setMenuOpen]= useState(false)
     return (
         <div className="app">
         <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
         <div className="sections">
             <Intro/>
             <Portfolio/>
