945fcc9472248f93cda394a2da122b67a95d23d2
AJPascoe <pascoe312@gmail.com>
2021-09-10 10:44:33 +0100

    adding dynamic active class

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -1,9 +1,71 @@
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss";
 
 export default function Portfolio() {
-    return (
-        <div className="portfolio" id ="portfolio">
-    
+  const [selected, setSelected] = useState("featured");
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+    {
+      id: "design",
+      title: "Design",
+    },
+    {
+      id: "content",
+      title: "Content",
+    },
+  ];
+
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img className="bankimg" src="assets/bankapp.jpg" alt="" />
+          <h3>Project 1</h3>
         </div>
-    )
+        <div className="item">
+          <img className="bankimg" src="assets/bankapp.jpg" alt="" />
+          <h3>Project 2</h3>
+        </div>
+        <div className="item">
+          <img className="bankimg" src="assets/bankapp.jpg" alt="" />
+          <h3>Project 3</h3>
+        </div>
+        <div className="item">
+          <img className="bankimg" src="assets/bankapp.jpg" alt="" />
+          <h3>Project 4</h3>
+        </div>
+        <div className="item">
+          <img className="bankimg" src="assets/bankapp.jpg" alt="" />
+          <h3>Project 5</h3>
+        </div>
+        <div className="item">
+          <img className="bankimg" src="assets/bankapp.jpg" alt="" />
+          <h3>Project 6</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
